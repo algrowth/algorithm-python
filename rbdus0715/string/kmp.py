57bc50bf86d3296127995b502cd2f4aa6e2fa880
Gyuyeon Cho <85426187+rbdus0715@users.noreply.github.com>
2023-09-22 09:12:09 +0900

    Update kmp.py

--- rbdus0715/string/kmp.py
+++ rbdus0715/string/kmp.py
@@ -40,3 +40,39 @@ def kmpSearch(H, N):
         
         
 print(kmpSearch(H, N))
+
+
+# parent = input()
+# pattern = input()
+
+# def failure(pattern):
+#     patternSize = len(pattern)
+#     ret = [0] * patternSize
+#     j = 0
+#     for i in range(1, patternSize):
+#         while j > 0 and pattern[i] != pattern[j]:
+#             j = ret[j-1]
+#         if pattern[i] == pattern[j]: 
+#             j += 1
+#             ret[i] = j
+#     return ret
+
+# def kmp(parent, pattern):
+#     pi = failure(pattern)
+#     parentSize = len(parent)
+#     patternSize = len(pattern)
+#     result = []
+#     j = 0
+#     for i in range(parentSize):
+#         while j > 0 and parent[i] != pattern[j]:
+#             j = pi[j-1]
+#         if parent[i] == pattern[j]:
+#             j += 1
+#             if j == patternSize:
+#                 result.append(j-i+1)
+#                 j = pi[j-1]
+#     return result
+    
+# # abababadababacabad
+# # ababacaba
+# print(kmp(parent, pattern))
